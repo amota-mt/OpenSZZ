6e72cf60fdc07b34e50a13bdce7ba395248de059
Takaha Mino <takaha317@gmail.com>
2023-08-07 11:43:01 +0900

    エンドポイントをMain.javaに変更

---
+++ app/src/main/java/com/SZZ/app/Main.java
@@ -0,0 +1,23 @@
+package com.SZZ.app;
+
+public class Main {
+    public static void main(String[] args) {
+        args = new String[4];
+		args[0] = "-all";
+		args[1] = "https://github.com/apache/commons-bcel.git";
+		args[2] = "https://issues.apache.org/jira/projects/BCEL";
+		args[3] = "BCEL";
+
+        long startTime = System.currentTimeMillis();
+        
+        try {
+            SZZApplication.main(args);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        long endTime = System.currentTimeMillis();
+		System.out.println("処理時間：" + (endTime - startTime) + " ms");
+    }
+}
--- run.sh
+++ run.sh
@@ -5,4 +5,7 @@
 #SBATCH --job-name openszz
 
 module load java
-sh ./gradlew run --args="-all https://github.com/apache/commons-bcel.git https://issues.apache.org/jira/projects/BCEL BCEL"
+
+sh ./gradlew run
+# sh ./gradlew run --args="-all https://github.com/apache/commons-bcel.git https://issues.apache.org/jira/projects/BCEL BCEL"
+# java -jar ./app/build/libs/app.jar -all https://github.com/apache/commons-bcel.git https://issues.apache.org/jira/projects/BCEL BCEL
